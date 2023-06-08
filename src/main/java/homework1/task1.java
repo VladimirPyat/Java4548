1149ca8be0f99b9176f391790858383971fde13f
Vladimir Pyat <scared@mail.ru>
2023-06-09 00:56:27 +0300

    task2

--- src/main/java/homework1/task1.java
+++ src/main/java/homework1/task1.java
@@ -1,6 +1,7 @@
 package homework1;
 
 public class task1 {
+//    1. Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
     public static void main(String[] args) {
         int[] userArray = new int[]{1, 11, 0, 1, 3, 2, 8, 1};
         int max = userArray[0];
